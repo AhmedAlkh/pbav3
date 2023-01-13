c814f5aa3dd73fa71460193a3d3fe8a49786176f
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-01-13 17:01:03 -0500

    changed form submission method

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Function for sending email through contact form
 
-function sendEmail() {
-    var userEmail = document.getElementById("email").value;
-    var messageSent = document.getElementById("message").value;
-    var userName = document.getElementById("name").value;
+// function sendEmail() {
+//     var userEmail = document.getElementById("email").value;
+//     var messageSent = document.getElementById("message").value;
+//     var userName = document.getElementById("name").value;
 
-    Email.send({
-        SecureToken : "9067e191-3012-4842-b29e-3e9c9fea89c9",
-        To : 'alkhatib_ahmed@hotmail.com',
-        From : userEmail,
-        Subject : "message from Photos by Alaa",
-        Body : "Name: " + userName
-        + "<br> Email: " + userEmail
-        + "<br> Message: " + messageSent
-    }).then(
-      message => alert("Message sent. Thank you!")
-    );
-}
+//     Email.send({
+//         SecureToken : "f645a8db-2d6d-44fe-bb97-7064c9a09d74",
+//         To : 'alkhatibahmed1995@gmail.com',
+//         From : userEmail,
+//         Subject : "message from Photos by Alaa",
+//         Body : "Name: " + userName
+//         + "<br> Email: " + userEmail
+//         + "<br> Message: " + messageSent
+//     }).then(
+//       message => alert("Message sent. Thank you!")
+//     );
+// }
 
 // function for navbar to collapse the navbar menu when a link is clicked
 
